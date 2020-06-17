61a307accc8658f3c40b2e23e3518a216481f5ca
Nynke Niehof <nynkeniehof@gmail.com>
2020-06-17 17:18:17 +0200

    recalibrated, new maneuver values

--- src/servo_calibration/servo_calibration.ino
+++ src/servo_calibration/servo_calibration.ino
@@ -10,17 +10,26 @@ void setup() {
   servoRight.attach(12);
 
   // values needed for straight path
-  // servoLeft.writeMicroseconds(1680);
-  // servoRight.writeMicroseconds(1300);
+   servoLeft.writeMicroseconds(1680);
+   servoRight.writeMicroseconds(1300);
 
   // values needed for standing still
-//  servoLeft.writeMicroseconds(1490);
-//  servoRight.writeMicroseconds(1490);
+  servoLeft.writeMicroseconds(1500);
+  servoRight.writeMicroseconds(1500);
 
   // values needed for driving backwards
-  servoLeft.writeMicroseconds(1490);
-  servoRight.writeMicroseconds(1490);
-  delay(10000);
+  servoLeft.writeMicroseconds(1380);
+  servoRight.writeMicroseconds(1700);
+
+  // values needed for right 90 deg turn
+  servoLeft.writeMicroseconds(1680);
+  servoRight.writeMicroseconds(1700);
+  delay(520);
+
+  // values needed for left 90 deg turn
+  servoLeft.writeMicroseconds(1300);
+  servoRight.writeMicroseconds(1300);
+  delay(520);
 
   servoLeft.detach();
   servoRight.detach();
